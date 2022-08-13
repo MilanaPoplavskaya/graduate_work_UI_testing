4dc84db58d1f9fe1ee9483bda1debeb0f0f450ba
Milana Poplavskaya <milanin7147@gmail.com>
2022-08-13 10:52:15 +0400

    deleted spaces

--- cypress/support/commands_negative.ts
+++ cypress/support/commands_negative.ts
@@ -1,9 +1,7 @@
 import { PASSWORD_SELECTORS } from "../selectors/selectorsForNegativeTests/passwordSelector";
 import { ERROR_MESSAGE } from "../selectors/selectorsForNegativeTests/errorMessageSelectors";
-import { REGISTER_SELECTOR_REGISTRATION_PAGE } from "../selectors/selectorsForNegativeTests/registrationPageSelectors";
 import { PHONE_NUMBER } from "../selectors/selectorsForNegativeTests/phoneNumberSelectors";
 
-const { registerButton } = REGISTER_SELECTOR_REGISTRATION_PAGE;
 const { passwordErr, confirmPasswordErr, confirmPasswordField } = PASSWORD_SELECTORS;
 const { errorMessage } = ERROR_MESSAGE;
 const { phoneNumberErr } = PHONE_NUMBER;
@@ -12,11 +10,9 @@ Cypress.Commands.add("RegisterWithoutNumber", (): void => {
     cy.get(phoneNumberErr).should("be.visible");
 });
 Cypress.Commands.add("RegisterWithInvalidNumber", (expectedInvalidNumber: string): void => {
-
     cy.get(errorMessage).should("be.visible").and("contain.text", expectedInvalidNumber);
 });
 Cypress.Commands.add("RegisterWithoutSms", (expectedInvalidSms: string): void => {
-
     cy.get(errorMessage).should("be.visible").and("contain.text", expectedInvalidSms);
 });
 Cypress.Commands.add("RegisterWithoutVerification", (password: string, expectedRequiredField: string): void => {
